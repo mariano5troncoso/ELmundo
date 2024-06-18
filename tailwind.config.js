3fe3493efee41188c05462769f6aaf41bdd6b1de
mariano5troncoso <mariano.nicolas.troncoso@gmail.com>
2024-06-18 17:08:25 -0300

    contactos y mas

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
 }
-
